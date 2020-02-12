ce31b73fbf33d46f4ddddd0891a56a17914e0997
Nguyen <thuminh.nguyen@dxc.com>
2020-02-12 18:03:43 +0700

    reponsive

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,10 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
   }
-
 }
